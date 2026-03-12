31b7022912441587e1ec4d838147bbd5182f2ccb
CaptCaptain <55301501+CaptCaptain@users.noreply.github.com>
2026-03-12 04:19:51 +0000

    Express server by default will use port 8080 unless specified in .env file. Removed existing .env file.

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const server = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 server.get("/", (req, res) => {
 	res.send(`Welcome from ${PORT}!`);
